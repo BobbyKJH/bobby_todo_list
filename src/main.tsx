223a88d71c887b147d2623744fb60077c687884a
BobbyKJH <rlatnguscjsw@gmail.com>
2023-01-12 00:15:12 +0900

    Feat: Redux store
    1. 내용
    
    - redux store 사용
    
    2. Git Type
    
    1) Feat: 새로운 기능 개발
    
    2) Fix: 버그 등 수정
    
    3) Docs: 문서 내용 변경
    
    4) Style: 코드가 아닌 스타일에 관련된 수정
    
    5) Refactor: 리팩토링 코드

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,15 @@ import ReactDOM from "react-dom/client";
 
 import { BrowserRouter } from "react-router-dom";
 
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>
 );
