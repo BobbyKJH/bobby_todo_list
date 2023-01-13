e1e27b9d3d06d0b1aec347cbdd85e49e21cbef7a
BobbyKJH <rlatnguscjsw@gmail.com>
2023-01-13 22:52:12 +0900

    Refactor: Bobby Todo List
    1. 내용
    
    - Bobby Todo List 다시 재시작
    
    2. Git Type
    
    1) Feat: 새로운 기능 개발
    
    2) Fix: 버그 등 수정
    
    3) Docs: 문서 내용 변경
    
    4) Style: 코드가 아닌 스타일에 관련된 수정
    
    5) Refactor: 리팩토링 코드

---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import App from "./App";
+import store from "./store/store";
+import { persistStore } from "redux-persist";
+import { BrowserRouter } from "react-router-dom";
+
+export const persistor = persistStore(store);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </BrowserRouter>
+);
