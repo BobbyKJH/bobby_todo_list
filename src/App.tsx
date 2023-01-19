a8759ff936b2cffb90bba5f4ad3df7344593e6ae
BobbyKJH <rlatnguscjsw@gmail.com>
2023-01-19 14:26:53 +0900

    Feat: App.tsx
    1. 내용
    
    - Route 연결
    
    2. Git Type
    
    1) Feat: 새로운 기능 개발
    
    2) Fix: 버그 등 수정
    
    3) Docs: 문서 내용 변경
    
    4) Style: 코드가 아닌 스타일에 관련된 수정
    
    5) Refactor: 리팩토링 코드

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 
+import { Route, Routes } from "react-router-dom";
+
+import DayPage from "./page/DayPage";
+import MonthPage from "./page/MonthPage";
+import YearPage from "./page/YearPage";
+
 const App = () => {
-  return <React.Fragment></React.Fragment>;
+  return (
+    <React.Fragment>
+      <Routes>
+        <Route path="/" element={<DayPage />} />
+        <Route path="/month" element={<MonthPage />} />
+        <Route path="/year" element={<YearPage />} />
+      </Routes>
+    </React.Fragment>
+  );
 };
 
 export default App;
