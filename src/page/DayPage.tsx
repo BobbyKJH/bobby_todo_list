1bd0f033bb29b3cb239d3cba7a45bae0602b6c7b
BobbyKJH <rlatnguscjsw@gmail.com>
2023-01-24 00:03:02 +0900

    Feat: DayTodo 추가 function 생성
    1. 내용
    
    - DayTodo 추가 Function Form 생성
    
    2. Git Type
    
    1) Feat: 새로운 기능 개발
    
    2) Fix: 버그 등 수정
    
    3) Docs: 문서 내용 변경
    
    4) Style: 코드가 아닌 스타일에 관련된 수정
    
    5) Refactor: 리팩토링 코드

--- src/page/DayPage.tsx
+++ src/page/DayPage.tsx
@@ -1,10 +1,7 @@
-import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import TodoList from "../components/common/TodoList";
 import useTodo from "../hooks/useTodo";
-import { increment } from "../store/slice/countSlice";
 import { dayTodo, TodoArray } from "../store/slice/todoSlice";
-import useSave from "../hooks/useSave";
 
 interface todo {
   f: string[];
@@ -16,14 +13,17 @@ const DayPage = () => {
   const { day } = useSelector(TodoArray);
   const { todo, onChangeTodo } = useTodo();
 
-  const dayListAdd = () => {
+  const dayListAdd = (e: React.ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
     dispatch(dayTodo([todo, ...day]));
   };
 
   return (
     <div>
-      <input type="text" value={todo} onChange={onChangeTodo} />
-      <button onClick={dayListAdd}>추가</button>
+      <form onSubmit={dayListAdd}>
+        <input type="text" value={todo} onChange={onChangeTodo} />
+        <button type="submit">추가</button>
+      </form>
 
       <TodoList todo={day} />
     </div>
