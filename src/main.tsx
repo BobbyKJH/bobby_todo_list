3238acf6f0bc7ea3e000e6ac1ada7c2c2ea7c310
BobbyKJH <rlatnguscjsw@gmail.com>
2023-01-31 22:42:59 +0900

    Docs: main.tsx 정리
    1. 내용
    
    - import 순서 정리
    
    2. Git Type
    
    1) Feat: 새로운 기능 개발
    
    2) Fix: 버그 등 수정
    
    3) Docs: 문서 내용 변경
    
    4) Style: 코드가 아닌 스타일에 관련된 수정
    
    5) Refactor: 리팩토링 코드

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import ReactDOM from "react-dom/client";
 
+import { BrowserRouter } from "react-router-dom";
+
 import { Provider } from "react-redux";
 
 import { persistStore } from "redux-persist";
 import { PersistGate } from "redux-persist/integration/react";
 
 import store from "./store/store";
-
-import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 
